0008198f95b03ef5fc04f1da722ffc826b5f6aa7
IgorAdelino <igoradelino@gmail.com>
2023-11-26 20:27:12 -0300

    new tests e2e

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { AppModule } from '@/infra/app.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
@@ -7,51 +8,44 @@ import request from 'supertest'
 import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
 
-describe('Fetch recent question (E2E)', () => {
+describe('Get question by slug (E2E)', () => {
   let app: INestApplication
-  let jwt: JwtService
-  let questionFactory: QuestionFactory
   let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [QuestionFactory, StudentFactory],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
+
+    jwt = moduleRef.get(JwtService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
-    jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
-  test('[GET] /questions', async () => {
+  test('[GET] /questions/:slug', async () => {
     const user = await studentFactory.makePrismaStudent()
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    await Promise.all([
-      questionFactory.makePrismaQuestion({
-        authorId: user.id,
-        title: 'Question-01',
-      }),
-      questionFactory.makePrismaQuestion({
-        authorId: user.id,
-        title: 'Question-02',
-      }),
-    ])
+    await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'Question-01',
+      slug: Slug.create('question-01'),
+    })
 
     const response = await request(app.getHttpServer())
-      .get('/questions')
+      .get('/questions/question-01')
       .set('Authorization', `Bearer ${accessToken}`)
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      questions: [
-        expect.objectContaining({ title: 'Question-02' }),
-        expect.objectContaining({ title: 'Question-01' }),
-      ],
+      question: expect.objectContaining({ title: 'Question-01' }),
     })
   })
 })
